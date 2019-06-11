95da199ecf23c98d9c7421f2a4138b38919f7445
Dinesh <dineshd.ttl@tatamotors.com>
2019-06-11 15:33:52 +0530

    add new comment

--- TravisDemo/TravisDemo/ViewController.swift
+++ TravisDemo/TravisDemo/ViewController.swift
@@ -13,8 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        #if PROD
+        //My_Target_Codes
+        //print(" IN Prod")
+        #else
+        //My_Other_Target_Codes
+        print(" IN Dev")
+        #endif
 
+    }
 
 }
 
