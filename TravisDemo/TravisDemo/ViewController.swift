aa974539824524c4cb4051d57229e7fddac46189
Dinesh <dineshd.ttl@tatamotors.com>
2019-06-12 12:16:42 +0530

    add

--- TravisDemo/TravisDemo/ViewController.swift
+++ TravisDemo/TravisDemo/ViewController.swift
@@ -13,9 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("dsds")
-        print("dsds")
-        print("dsds")
     }
 
 }
