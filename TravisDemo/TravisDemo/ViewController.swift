1b18b64d73114ad6838d69e5ca088160601770f4
Dinesh <dineshd.ttl@tatamotors.com>
2019-06-13 12:50:55 +0530

    change ymal file

--- TravisDemo/TravisDemo/ViewController.swift
+++ TravisDemo/TravisDemo/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("update gitignor 02")
         print("update gitignor 02")
+        print("update gitignor 02")
+
     }
 
 }
